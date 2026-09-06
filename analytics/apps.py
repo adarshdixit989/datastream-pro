b53959190c5253762508848e85ed55b38b10e458
adarshdixit989 <119063821+adarshdixit989@users.noreply.github.com>
2026-09-06 19:09:01 +0530

    fix: initialize SQLite analytics tables on demo deployment

--- analytics/apps.py
+++ analytics/apps.py
@@ -3,3 +3,19 @@ from django.apps import AppConfig
 class AnalyticsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "analytics"
+
+    def ready(self):
+        from django.conf import settings
+        if settings.DATABASES["default"]["ENGINE"] != "django.db.backends.sqlite3":
+            return
+        try:
+            from django.db import connection
+            from .models import Anomaly, Forecast
+            tables = set(connection.introspection.table_names())
+            with connection.schema_editor() as schema_editor:
+                if Anomaly._meta.db_table not in tables:
+                    schema_editor.create_model(Anomaly)
+                if Forecast._meta.db_table not in tables:
+                    schema_editor.create_model(Forecast)
+        except Exception:
+            pass
